528f0332e70a3ae33f6aa0fac145151311642bae
brahdee <braddy@peralta.sh>
2025-12-15 04:58:48 -0500

    Redesign website for brevity

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    $('#turnyellow').mouseover(function() {
-        $('body').css('background-color', '#f4f2ee');
-    }).mouseout(function() {
-        $('body').css('background-color', '#f0f2f6');
-    });;
-});
